b341e3292ccc51d21ad8a62be1e5e440e37a7b47
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-16 20:50:38 +0200

    injection, event emitter

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,23 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import {LoggingService} from '../logging.service';
+import { Component } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe((status: string) => alert('New Status: ' + status));
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
-    this.loggingService.logStatusChange(accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
   }
 }
