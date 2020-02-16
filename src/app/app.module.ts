7624f3504e7abc94ca80b4e56ecc3ff243467289
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-16 21:04:26 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
-import { InactiveUsersComponent } from './account/inactive-users/inactive-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
 
 @NgModule({
   declarations: [
